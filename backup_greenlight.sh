05a874ea808de054431a2b19a1bb2fd14a7040ed
Aleksei Ivanov <contact@aleksei.dev>
2022-06-25 18:11:47 +0300

    Update backup_greenlight.sh

--- backup_greenlight.sh
+++ backup_greenlight.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+backups_dir="backups/mysql"
+
 cd $(dirname $BASH_SOURCE)
-docker-compose exec -T postgres pg_dumpall -c -U postgres > backups/greenlight/`date +%d-%m-%Y"_"%H_%M_%S`.sql
+docker-compose exec -T postgres pg_dumpall -c -U postgres > $backups_dir/`date +%d-%m-%Y"_"%H_%M_%S`.sql
+
+# Keep 5 most recent backups
+ls -tp $backups_dir | grep -v '/$' | tail -n +6 | xargs -I {} rm -- $backups_dir/{}
 
 endpoint="${AWS_ENDPOINT:-https://storage.yandexcloud.net}"
 profile="backup-uploader@fmp-msu-yandex-cloud"
 bucket="fmp-msu-backups"
 
-aws s3 --endpoint $endpoint --profile $profile sync backups/greenlight s3://fmp-msu-backups/metal/home/fmp/bbb/backups/greenlight
+aws s3 --endpoint $endpoint --profile $profile sync $$backups_dir s3://fmp-msu-backups/metal/home/fmp/bbb/$$backups_dir
 
